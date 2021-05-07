e392a040284ff9e719deb5efa26bb325fda4cba7
James Wesley Goedert <jwgoedert@gmail.com>
2021-05-07 18:28:03 -0500

    Added adventurers array

--- scripts.js
+++ scripts.js
@@ -1,2 +1,36 @@
-This is some test script
+console.log('hello adventurers!');
 
+let adventurers = [
+    {
+        firstName: 'Richard',
+        lastName: 'Daniel',
+        favoriteColor: 'blue',
+        livesInOmaha: true,
+        age: 31,
+        interests: ['Reading', 'Working out', 'Hanging with Friends', 'Video games']
+    },
+    {
+        firstName: 'Seiko',
+        lastName: 'Kitajima',
+        favoriteColor: 'orange',
+        livesInOmaha: true,
+        age: 31,
+        interests: ['Cooking']
+    },
+    {
+        firstName: 'Lauren',
+        lastName: 'Vosler',
+        favoriteColor: 'pink',
+        livesInOmaha: true,
+        age: 22,
+        interests: ['Reading', 'Working out', 'Hanging with Friends', 'Video games']
+    },
+    {
+        firstName: 'James',
+        lastName: 'Goedert',
+        favoriteColor: 'green',
+        livesInOmaha: false,
+        age: 35,
+        interests: ['Drawing', 'Working with my hands', 'coding', 'reading']
+    }
+];
